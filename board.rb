f9790b97435bc043d72c48630ccafd6e4a92af0e
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-08 11:53:59 -0700

    Add methods for printing the board

--- board.rb
+++ board.rb
@@ -9,7 +9,20 @@ class Board
     col_position = @game_board[row_position].index(position)
     [row_position, col_position]
   end
+
+  def get_row_string(row)
+    " #{@game_board[row][0]} | #{@game_board[row][1]} | #{@game_board[row][2]}"
+  end
+
+  def print_board
+    separator = '---+---+---'
+    puts get_row_string(0)
+    puts separator
+    puts get_row_string(1)
+    puts separator
+    puts get_row_string(2)
+  end
 end
 
 my_board = Board.new
-p my_board.board_search('X')
+my_board.print_board
