2dd0f695f7d340c5253121077f5da2394c6eae3a
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-10 13:50:45 -0700

    Refactor game loop. Remove stdin because of conflicts in replit

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -6,15 +6,15 @@ loop do
   new_game = Game.new
   puts 'Welcome to my Tic-Tac-Toe game!'
   puts "Player 1 will use 'X'. Player 2 will 'O'."
-  puts 'Connect 3 symbols to win! Press any key to continue >>'
-  $stdin.getch
+  puts 'Connect 3 symbols to win! Press enter to continue >>'
+  gets.chomp
   new_game.play_game
   new_game.determine_winner
   case new_game.winner
   when 'Player 1'
-    puts 'Congratulations Player 1!'
+    puts "Congratulations Player 1! You've won the game"
   when 'Player 2'
-    puts 'Congratulations Player 2!'
+    puts "Congratulations Plyaer 2! You've won the game"
   else
     puts 'This game was a draw'
   end
