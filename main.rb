8a9be4dfadde52c3c6c081a90f68cfab4c69a97b
jmsmith1018 <jmsmith1018@gmail.com>
2021-11-10 10:54:00 -0700

    Add introduction to game loop

--- main.rb
+++ main.rb
@@ -4,6 +4,10 @@ require_relative('game')
 
 loop do
   new_game = Game.new
+  puts 'Welcome to my Tic-Tac-Toe game!'
+  puts "Player 1 will use 'X'. Player 2 will 'O'."
+  puts 'Connect 3 symbols to win! Press any key to continue >>'
+  $stdin.getch
   new_game.play_game
   new_game.determine_winner
   case new_game.winner
