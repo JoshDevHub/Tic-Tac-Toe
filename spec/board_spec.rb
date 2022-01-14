92555ce48339a338ade201363b682901d2bf90c7
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-14 16:27:14 -0700

    Add more passing tests to #place_token

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -25,27 +25,40 @@ describe Board do # rubocop: disable Metrics/BlockLength
     end
   end
 
-  describe '#place_token' do
+  describe '#place_token' do # rubocop: disable Metrics/BlockLength
     subject(:playing_board) { Board.new }
-    xit 'sends board_search to self and returns a board position' do
 
+    context 'places a token on the first position on the board' do
+      position = 1
+      before do
+        allow(playing_board).to receive(:board_search).with(position).and_return([0, 0])
+      end
+
+      it "changes the element in the board at [0][0] to 'X'" do
+        token = 'X'
+        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[0][0] }.to(token)
+      end
+
+      it "changes the element in the board at [0][0] to 'O'" do
+        token = '0'
+        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[0][0] }.to(token)
+      end
     end
 
-    context 'places tokens on the board' do
+    context 'places a token in the middle of the board' do
+      position = 5
       before do
-        allow(playing_board).to receive(:board_search).and_return([0, 0])
+        allow(playing_board).to receive(:board_search).with(position).and_return([1, 1])
       end
 
-      it "changes the requested element in the board to 'X'" do
+      it "changes the element in the board at [1][1] to 'X'" do
         token = 'X'
-        position = [0, 0]
-        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[0][0] }.to(token)
+        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[1][1] }.to(token)
       end
 
-      it "changes the requested element in the boat to 'O'" do
-        token = '0'
-        position = [0, 0]
-        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[0][0] }.to(token)
+      it "changes the element in the board at [1][1] to 'O'" do
+        token = 'O'
+        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[1][1] }.to(token)
       end
     end
   end
