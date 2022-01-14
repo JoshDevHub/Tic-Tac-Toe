774a0e08f91a78aad46aafaf38d4ac4d400e9f49
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-14 11:42:53 -0700

    Add first test for #check_full_board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,5 +5,11 @@
 require './lib/board'
 
 describe Board do
-  
+  describe '#check_full_board' do
+    subject(:game_board) { Board.new }
+
+    it 'returns false when the game board is empty' do
+      expect(game_board.check_full_board).to eq(false)
+    end
+  end
 end
