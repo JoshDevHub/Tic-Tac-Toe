ca13bb6da1e7fa90ff7ad3666235c7398b8e7635
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-14 15:59:06 -0700

    Add passing tests for #place_token in the context of changing the state of the game board

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,7 @@
 
 require './lib/board'
 
-describe Board do
+describe Board do # rubocop: disable Metrics/BlockLength
   describe '#full_board?' do
     subject(:playing_board) { Board.new }
 
@@ -22,6 +22,31 @@ describe Board do
       full_board = playing_board.game_board.map { |row| row.map { |_square| 'X' } }
       allow(playing_board).to receive(:game_board).and_return(full_board)
       expect(playing_board.full_board?).to eq(true)
+    end
+  end
+
+  describe '#place_token' do
+    subject(:playing_board) { Board.new }
+    xit 'sends board_search to self and returns a board position' do
+
+    end
+
+    context 'places tokens on the board' do
+      before do
+        allow(playing_board).to receive(:board_search).and_return([0, 0])
+      end
+
+      it "changes the requested element in the board to 'X'" do
+        token = 'X'
+        position = [0, 0]
+        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[0][0] }.to(token)
+      end
+
+      it "changes the requested element in the boat to 'O'" do
+        token = '0'
+        position = [0, 0]
+        expect { playing_board.place_token(token, position) }.to change { playing_board.game_board[0][0] }.to(token)
+      end
     end
   end
 end
