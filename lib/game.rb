34b025599c33cb56e0e8ce1f32e30006a9786463
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-16 09:33:59 -0700

    Refactor Game class to leverage getters/setters

--- lib/game.rb
+++ lib/game.rb
@@ -5,7 +5,8 @@ require_relative('board')
 
 # game.rb
 class Game
-  attr_accessor :winner
+  attr_reader :player_one, :player_two, :game_board
+  attr_accessor :winner, :game_over
 
   def initialize
     @player_one = Player.new('X', 'Player 1')
@@ -16,44 +17,44 @@ class Game
   end
 
   def play_game
-    until @game_over
-      play_round(@player_one)
-      @player_one.winner = @game_board.check_win_condition('X')
-      @game_over = @player_one.winner || @game_board.check_full_board
-      break if @game_over
+    until game_over
+      play_round(player_one)
+      player_one.winner = game_board.win?('X')
+      self.game_over = player_one.winner || game_board.full_board?
+      break if game_over
 
-      play_round(@player_two)
-      @player_two.winner = @game_board.check_win_condition('O')
-      @game_over = @player_two.winner
+      play_round(player_two)
+      player_two.winner = game_board.win?('O')
+      self.game_over = player_two.winner
     end
+  end
+
+  def determine_winner
+    self.winner = 'Player 1' if player_one.winner
+    self.winner = 'Player 2' if player_two.winner
   end
 
   private
 
   def play_round(player)
-    @game_board.print_board
+    game_board.print_board
     query = "#{player.name}: What is your choice?"
     player_input = player.take_input(query)
     sanitized_input = validate_input(player_input, player)
-    @game_board.place_token(player.token, sanitized_input)
+    game_board.place_token(player.token, sanitized_input)
   end
 
   def validate_input(input, player)
-    return input if @game_board.game_board.flatten.include?(input)
+    return input if game_board.game_board.flatten.include?(input)
 
     is_valid = false
     error_query = 'Your choice was invalid. Please pick a valid board position.'
     correct_input = ''
     until is_valid
       retry_input = player.take_input(error_query)
-      is_valid = @game_board.game_board.flatten.include?(retry_input)
+      is_valid = game_board.game_board.flatten.include?(retry_input)
       correct_input = retry_input
     end
     correct_input
-  end
-
-  def determine_winner
-    @winner = 'Player 1' if @player_one.winner
-    @winner = 'Player 2' if @player_two.winner
   end
 end
