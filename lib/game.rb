5d2a763a17c23964089d6af77aa8cbf02c8a711f
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-18 09:58:27 -0700

    Refactor of game to set up for improved testing

--- lib/game.rb
+++ lib/game.rb
@@ -6,24 +6,44 @@ require_relative('board')
 # game.rb
 class Game
   attr_reader :player_one, :player_two, :game_board
-  attr_accessor :winner, :game_over
 
   def initialize
     @player_one = Player.new('X', 'Player 1')
     @player_two = Player.new('O', 'Player 2')
-    @game_board = Board.new
-    @game_over = false
-    @winner = nil
   end
 
   def play_game
+    introduction
+    loop do
+      @game_board = Board.new
+      game_loop
+      puts determine_result
+      break unless play_again?
+    end
+  end
+
+  private
+
+  def introduction
+    puts <<~HEREDOC
+      Welcome to a game of Tic-Tac-Toe!
+      Player 1 will use an 'X' symbol and Player 2 will use 'O'.
+      Connect 3 symbols to win! Press Enter to continue >>
+    HEREDOC
+    gets.chomp
+  end
+
+  def game_loop
     until game_over?
       play_round(player_one)
       break if game_over?
 
       play_round(player_two)
     end
-    puts determine_result
+  end
+
+  def game_over?
+    game_board.win?('X') || game_board.win?('O') || game_board.full_board?
   end
 
   def determine_result
@@ -36,18 +56,18 @@ class Game
     end
   end
 
-  private
-
-  def game_over?
-    game_board.win?('X') || game_board.win?('O') || game_board.full_board?
-  end
-
   def play_round(player)
     game_board.print_board
     query = "#{player.name}: What is your choice?"
     player_input = player.take_input(query)
     sanitized_input = validate_input(player_input, player)
     game_board.place_token(player.token, sanitized_input)
+  end
+
+  def play_again?
+    puts 'Would you like to play again? Y/n'
+    response = gets.chomp.downcase
+    response == 'y'
   end
 
   def validate_input(input, player)
--- main.rb
+++ main.rb
@@ -2,14 +2,4 @@
 
 require_relative('lib/game')
 
-loop do
-  new_game = Game.new
-  puts 'Welcome to my Tic-Tac-Toe game!'
-  puts "Player 1 will use 'X'. Player 2 will use 'O'."
-  puts 'Connect 3 symbols to win! Press enter to continue >>'
-  gets.chomp
-  new_game.play_game
-  puts 'Would you like to play again? Y/n'
-  answer = gets.chomp.downcase
-  break unless answer == 'y'
-end
+Game.new.play_game
