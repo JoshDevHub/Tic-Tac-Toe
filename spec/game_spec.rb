36260270eb3f76a6dcae52ea5666e33f230e37c5
jmsmith1018 <jmsmith1018@gmail.com>
2022-01-18 10:10:51 -0700

    Add tests checking that #play_game stops when #play_again? is false

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,7 +6,30 @@ require './lib/game'
 
 describe Game do
   describe '#play_game' do
-    # placeholder
+    subject(:tic_tac_toe) { described_class.new }
+    context 'when players play two games' do
+      before do
+        allow(tic_tac_toe).to receive(:game_over?).and_return(true)
+        allow(tic_tac_toe).to receive(:play_again?).and_return(true, false)
+      end
+
+      it 'calls game_loop twice' do
+        expect(tic_tac_toe).to receive(:game_loop).twice
+        tic_tac_toe.play_game
+      end
+    end
+
+    context 'when players play five games' do
+      before do
+        allow(tic_tac_toe).to receive(:game_over?).and_return(true)
+        allow(tic_tac_toe).to receive(:play_again?).and_return(true, true, true, true, false)
+      end
+
+      it 'calls game_loop five times' do
+        expect(tic_tac_toe).to receive(:game_loop).exactly(5).times
+        tic_tac_toe.play_game
+      end
+    end
   end
 
   describe '#determine_result' do
