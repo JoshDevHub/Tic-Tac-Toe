a67f7fee39470d02114dcaa1df5134e95ec1c0ef
Josh Smith <jmsmith1018@gmail.com>
2023-12-22 14:54:27 -0800

    Add `swap_players` method to change player control in `Game`

--- lib/game.rb
+++ lib/game.rb
@@ -2,87 +2,28 @@ require_relative("player")
 require_relative("board")
 
 class Game
-  attr_reader :player_one, :player_two, :game_board
+  attr_reader :current_player
 
   def initialize(player_one:, player_two:, board:)
     @player_one = player_one
     @player_two = player_two
     @board = board
+    @current_player = player_one
   end
 
   def over?
     @board.win?("X") || @board.win?("O") || @board.full_board?
   end
 
-  def play_game
-    introduction
-    loop do
-      @game_board = Board.new
-      game_loop
-      puts determine_result
-      break unless play_again?
+  def winner
+    if @board.win?("X")
+      @player_one
+    elsif @board.win?("O")
+      @player_two
     end
   end
 
-  private
-
-  def introduction
-    puts <<~HEREDOC
-      Welcome to a game of Tic-Tac-Toe!
-      Player 1 will use an 'X' symbol and Player 2 will use 'O'.
-      Connect 3 symbols to win! Press Enter to continue >>
-    HEREDOC
-    gets.chomp
-  end
-
-  def game_loop
-    until game_over?
-      play_round(player_one)
-      break if game_over?
-
-      play_round(player_two)
-    end
-  end
-
-  def game_over?
-    game_board.win?("X") || game_board.win?("O") || game_board.full_board?
-  end
-
-  def determine_result
-    if game_board.full_board?
-      "This game was a draw."
-    elsif game_board.win?("X")
-      "Congratulations Player 1! You've won the game"
-    else
-      "Congratulations Player 2! You've won the game"
-    end
-  end
-
-  def play_round(player)
-    game_board.print_board
-    query = "#{player.name}: What is your choice?"
-    player_input = player.take_input(query)
-    sanitized_input = validate_input(player_input, player)
-    game_board.place_token(player.token, sanitized_input)
-  end
-
-  def play_again?
-    puts "Would you like to play again? Y/n"
-    response = gets.chomp.downcase
-    response == "y"
-  end
-
-  def validate_input(input, player)
-    return input if game_board.game_board.flatten.include?(input)
-
-    is_valid = false
-    error_query = "Your choice was invalid. Please pick a valid board position."
-    correct_input = ""
-    until is_valid
-      retry_input = player.take_input(error_query)
-      is_valid = game_board.game_board.flatten.include?(retry_input)
-      correct_input = retry_input
-    end
-    correct_input
+  def swap_players
+    @current_player = current_player == @player_one ? @player_two : @player_one
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -34,4 +34,50 @@ describe Game do
       end
     end
   end
+
+  describe "#winner" do
+    context "when there is no winner" do
+      it "returns `nil`" do
+        expect(tic_tac_toe.winner).to be_nil
+      end
+    end
+
+    context "when player_one is the winner" do
+      before do
+        allow(board).to receive(:win?).with("X").and_return true
+      end
+
+      it "returns player_one" do
+        expect(tic_tac_toe.winner).to eq player_one
+      end
+    end
+
+    context "when player_two is the winner" do
+      before do
+        allow(board).to receive(:win?).with("O").and_return true
+      end
+
+      it "returns player_two" do
+        expect(tic_tac_toe.winner).to eq player_two
+      end
+    end
+  end
+
+  describe "#current_player" do
+    context "when the current_player is player_one" do
+      it "changes the current player to player_two" do
+        expect { tic_tac_toe.swap_players }
+          .to change(tic_tac_toe, :current_player).from(player_one).to(player_two)
+      end
+    end
+
+    context "when the current_player is player_two" do
+      before { tic_tac_toe.swap_players }
+
+      it "changes the current player to player_one" do
+        expect { tic_tac_toe.swap_players }
+          .to change(tic_tac_toe, :current_player).from(player_two).to(player_one)
+      end
+    end
+  end
 end
